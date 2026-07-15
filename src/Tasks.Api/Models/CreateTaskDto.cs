d851be0f68ba0995fdf5f3ef980e2aac29699520
Oleksandr Andriienko <sashaandrzb@gmail.com>
2026-07-15 16:12:12 +0300

    feat: add validation for Title property in CreateTaskDto

--- src/Tasks.Api/Models/CreateTaskDto.cs
+++ src/Tasks.Api/Models/CreateTaskDto.cs
@@ -1,7 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Tasks.Api.Models;
 
 public class CreateTaskDto
 {
+    [Required(ErrorMessage = "Title is required.")]
     public required string Title { get; set; }
 
     public string? Description { get; set; }
