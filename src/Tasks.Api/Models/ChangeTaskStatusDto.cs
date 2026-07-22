78d1b8794737ae7299ce37caf40c5d918e38f806
Oleksandr Andriienko <sashaandrzb@gmail.com>
2026-07-22 15:02:25 +0300

    fix: remove redundant converting attribute

--- src/Tasks.Api/Models/ChangeTaskStatusDto.cs
+++ src/Tasks.Api/Models/ChangeTaskStatusDto.cs
@@ -1,12 +1,9 @@
 using System.ComponentModel.DataAnnotations;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Converters;
 
 namespace Tasks.Api.Models;
 
 public class ChangeTaskStatusDto
 {
-    [JsonConverter(typeof(StringEnumConverter))]
     [Required]
     public TaskItemStatus Status { get; set; }
 }
