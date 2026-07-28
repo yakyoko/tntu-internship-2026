8ac65341e8655ec6d947c9640a8f0c2ef1def4f9
Oleksandr Andriienko <sashaandrzb@gmail.com>
2026-07-28 12:58:35 +0300

    feat: update service and repository method to use TaskItemStatus enum for filtering

--- src/Tasks.Api/Interfaces/ITaskRepository.cs
+++ src/Tasks.Api/Interfaces/ITaskRepository.cs
@@ -6,7 +6,10 @@ public interface ITaskRepository
 {
     Task CreateTaskAsync(TaskItem task);
     Task<TaskItem?> GetTaskByIdAsync(Guid projectId, Guid taskId);
-    Task<IEnumerable<TaskItem>> GetAllTasksByProjectIdAsync(Guid projectId);
+    Task<IEnumerable<TaskItem>> GetAllTasksByProjectIdAsync(
+        Guid projectId,
+        TaskItemStatus? status = null
+    );
     Task SaveChangesAsync();
     Task RemoveTaskAsync(TaskItem task);
 }
--- src/Tasks.Api/Interfaces/ITaskService.cs
+++ src/Tasks.Api/Interfaces/ITaskService.cs
@@ -6,7 +6,10 @@ public interface ITaskService
 {
     Task<TaskItemDto> CreateTaskAsync(Guid projectId, CreateTaskDto createTaskDto);
     Task<TaskItemDto> GetTaskByIdAsync(Guid projectId, Guid taskId);
-    Task<IEnumerable<TaskItemDto>> GetAllTasksByProjectIdAsync(Guid projectId);
+    Task<IEnumerable<TaskItemDto>> GetAllTasksByProjectIdAsync(
+        Guid projectId,
+        TaskItemStatus? taskFilterStatus
+    );
     Task<TaskItemDto> UpdateTaskAsync(Guid projectId, Guid taskId, UpdateTaskDto updateTaskDto);
     Task<TaskItemDto> ChangeTaskStatusAsync(
         Guid projectId,
